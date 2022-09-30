82aae9b2aeca569c0c28a87fb6a504206ace2812
akoval29 <akoval29@gmail.com>
2022-10-01 00:05:00 +0300

    ref

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
+ReactDOM
+    .createRoot(document.getElementById('root'))
+    .render(
     <React.StrictMode>
-        <App />
+        <App/>
     </React.StrictMode>,
-    document.getElementById('root')
 );
