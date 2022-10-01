cd7dc335dcf2e1ef057d1b028968ad8c42f33468
akoval29 <akoval29@gmail.com>
2022-10-02 02:38:55 +0300

    default setup done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider, createTheme } from "@mui/material";
 
 import './index.css';
 import App from './components/App';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#61dafb'
+        },
+        secondary: {
+            main: '#00ff00'
+        }
+    }
+});
 
 ReactDOM
     .createRoot(document.getElementById('root'))
     .render(
     <React.StrictMode>
-        <App/>
+
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+
+        {/* <App/> */}
     </React.StrictMode>,
 );
